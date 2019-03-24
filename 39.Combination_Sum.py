7246fcbe0b07bc28e18ca02297cdf80c769ca61d
Nian <40905820+learlinian@users.noreply.github.com>
2019-03-24 11:59:23 -0500

    Update 39.Combination_Sum.py
    add "candidates.sort()" in function combinationSum().

--- 39.Combination_Sum.py
+++ 39.Combination_Sum.py
@@ -4,25 +4,26 @@
 The same repeated number may be chosen from candidates unlimited number of times."""
 class Solution(object):
     def combinationSum(self, candidates, target):
-        self.result = []
         candidates.sort()
+        self.result = []
         self.findCombinator([], candidates, target)
         return self.result
 
-        def findCombinator(self, temp, candidates, target):     # iteration function to find all solutions
-            for item in candidates:
-                if item > target:   # break the loop if smallest item in candidates is greater than target
-                    break
-                temp.append(item)   # append the item
-                print(temp)
-                if item == target:
-                    self.result.append(temp.copy())     # append the result
-                    temp.pop()
-                    break
-                else:
-                    index = candidates.index(item)
-                    self.findCombinator(temp, candidates[index:], target - item)
-                temp.pop()      # pop item in temp list
+    def findCombinator(self, temp, candidates, target):  # iteration function to find all solutions
+        # if target >= 0:  # break the loop if target is smaller than 0
+        for item in candidates:
+            if item > target:  # break the loop if smallest item in candidates is greater than target
+                break
+            temp.append(item)  # append the item
+            print(temp)
+            if item == target:
+                self.result.append(temp.copy())  # append the result
+                temp.pop()
+                break
+            else:
+                index = candidates.index(item)
+                self.findCombinator(temp, candidates[index:], target - item)
+            temp.pop()  # pop item in temp list
 
 
 if __name__ == '__main__':
