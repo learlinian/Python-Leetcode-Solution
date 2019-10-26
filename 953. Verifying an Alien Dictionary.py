b24f759e510c1536ffd669775c73e1c69a341cde
Nian <40905820+learlinian@users.noreply.github.com>
2019-10-26 23:41:49 +0800

    Update 953. Verifying an Alien Dictionary.py
    ad d comments

--- 953. Verifying an Alien Dictionary.py
+++ 953. Verifying an Alien Dictionary.py
@@ -2,20 +2,20 @@ class Solution(object):
     def isAlienSorted(self, words, order):
         records = []
 
+        # find sequence pairs
         for i in range(len(words)-1):
             for j in range(min(len(words[i]), len(words[i+1]))):
                 if words[i][j] == words[i+1][j]:
                     continue
                 records.append([words[i][j], words[i+1][j]])
                 break
-
+        # return False if nothing collected
         if not records:
             return False
-
+        # check whether the sequence in order is correct
         for record in records:
             if order.index(record[0]) > order.index(record[1]):
                 return False
-
         return True
 
 
