40dda0035aeab334043150e96bef33f4171a7181
Nian <40905820+learlinian@users.noreply.github.com>
2019-10-27 23:38:11 +0800

    Update 269.Alien Dictionary.py
    update the sorting method

--- 269.Alien Dictionary.py
+++ 269.Alien Dictionary.py
@@ -1,9 +1,4 @@
 def Solution(words):
-    characters = set()
-    for word in words:
-        characters.update(word)
-
-    characters = list(characters)
     records = []
     for i in range(len(words)-1):
         for j in range(min(len(words[i]), len(words[i+1]))):
@@ -12,19 +7,40 @@ def Solution(words):
             records.append([words[i][j], words[i+1][j]])
             break
 
-    last_character = []
-    while last_character != characters:
-        last_character = characters.copy()
+    # invalid words
+    if not records:
+        return []
 
+    characters = set()
+    for word in words:
+        characters.update(word)
+    # characters = list(characters)
+    
+    # Bubble sorting -- time complexity: O(n^2)
+    # last_character = []
+    # while last_character != characters:
+    #     last_character = characters.copy()
+    #
+    #     for record in records:
+    #         first_index = characters.index(record[0])
+    #         second_index = characters.index(record[1])
+    #         if first_index > second_index:
+    #             characters[first_index], characters[second_index] = characters[second_index], characters[first_index]
+
+    # Topological sort -- time complexity: O(n^2)
+    overlapped_char = set(i[0] for i in records)  # all characters appear in the words
+    missing_key = (characters - overlapped_char).pop()
+    all_char = len(characters)
+    characters = [missing_key]
+
+    while len(characters) < all_char:
         for record in records:
-            first_index = characters.index(record[0])
-            second_index = characters.index(record[1])
-            if first_index > second_index:
-                characters[first_index], characters[second_index] = characters[second_index], characters[first_index]
+            if record[1] == characters[0]:
+                characters = [record[0]] + characters
 
     return characters
 
 
 if __name__ == '__main__':
-    words = ["baa", "abcd", "abca", "cab", "cad"]
+    words = ["wrt","wrf","er","ett","rftt"]
     print(Solution(words))
