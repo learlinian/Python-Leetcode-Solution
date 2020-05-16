636d629cd2087564b17c7fb00b1a86ed32549ce4
Nian <40905820+learlinian@users.noreply.github.com>
2020-05-16 15:54:01 +0800

    Add files via upload

---
+++ 81. Search in Rotated Sorted Array II.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def search(self, nums, target):
+        print(nums)
+        nums_len = len(nums)
+        if nums_len == 0:
+            return False
+        mid = nums_len // 2
+        result = False  # check whether checked array contains target
+        if nums[mid] == target:
+            return True
+        elif nums_len == 1:
+            return target == nums[0]
+        elif target < nums[mid]:  # target is smaller, need to find a larger value
+            if mid < nums_len - 1 and (nums[mid] > nums[mid + 1] or nums[mid + 1] > nums[-1]):
+                result = self.search(nums[mid + 1:].copy(), target)
+            return result if result is True else self.search(nums[:mid].copy(), target)
+        else:  # target is larger
+            if mid > 0 and (nums[mid - 1] > nums[mid] or nums[0] > nums[mid]):
+                result = self.search(nums[:mid].copy(), target)
+            return result if result is True else self.search(nums[mid + 1:].copy(), target)
+
+
+if __name__ == '__main__':
+    array = [4, 5, 6, 7, 0, 1, 2]
+    # target = 6
+    for target in [4, 5, 6, 7, 0, 1, 2, 10]:
+        print(Solution().search(array, target))
