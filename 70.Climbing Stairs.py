d307dbb18d267909f623cd794dd529b28c8b4180
nian <nian@outlook.com>
2021-10-05 19:35:37 +0800

    add dp solution

--- 70.Climbing Stairs.py
+++ 70.Climbing Stairs.py
@@ -10,16 +10,30 @@ class Solution(object):
             else:
                 for i in [1, 2]:
                     if val - i >= 0:
-                        if val-i in self.save:
-                            self.ways += self.save[val-i]
+                        if val - i in self.save:
+                            self.ways += self.save[val - i]
                         else:
-                            climb(val-i)
-                            self.save[val-i] = self.ways
+                            climb(val - i)
+                            self.save[val - i] = self.ways
 
         climb(n)
         return self.ways
 
 
+# runtime: 6%; memory: 64%
+# new solution: dynamic programming
+class Solution2(object):
+    def climbStairs(self, n):
+        if n == 1:
+            return 1
+        dp = [0] * 45
+        dp[0] = 1
+        dp[1] = 2
+        i = 2
+        while i < n:
+            dp[i] = dp[i - 1] + dp[i - 2]
+            i += 1
+        return dp[n - 1]
 
 
 if __name__ == '__main__':
