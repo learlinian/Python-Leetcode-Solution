a8b36a0201669e374a9832dc0a3674b8d1a607a0
nian <nian@outlook.com>
2021-10-06 21:22:40 +0800

    new file

--- 442. Find All Duplicates in an Array.py
+++ 442. Find All Duplicates in an Array.py
@@ -1,4 +1,26 @@
+# solution 1: hacking solution. Sort first then return answer
+# runtime: 70%; memory: 97%
 class Solution(object):
     def findDuplicates(self, nums):
         nums.sort()
-        return [nums[i] for i in range(1, len(nums)) if nums[i] == nums[i-1] and (i == 1 or nums[i] != nums[i-2])]
+        return [nums[i] for i in range(1, len(nums)) if nums[i] == nums[i - 1] and (i == 1 or nums[i] != nums[i - 2])]
+
+
+# solution 2: use tricky nums index position
+# runtime: 74%; memory: 70%
+class Solution2(object):
+    def findDuplicates(self, nums):
+        result = []
+
+        for num in nums:
+            if nums[abs(num) - 1] < 0:
+                result.append(abs(num))
+            else:
+                nums[abs(num) - 1] *= -1
+
+        return result
+
+
+if __name__ == '__main__':
+    nums = [4, 3, 2, 7, 8, 2, 3, 1]
+    print(Solution2().findDuplicates(nums))
