ce31552079103b303cd0e47ad4fc2dab0a4cad11
nian <nian@outlook.com>
2021-10-16 14:40:29 +0800

    refactor solution

--- 188. Best Time to Buy and Sell Stock IV.py
+++ 188. Best Time to Buy and Sell Stock IV.py
@@ -1,19 +1,21 @@
+# solution: iterative dp
+# runtime: 93%; memory: 87%
+
 class Solution(object):
     def maxProfit(self, k, prices):
         prices_len = len(prices)
         if k >= prices_len // 2:
             return sum([v - u for u, v in zip(prices, prices[1:]) if v - u >= 0])
 
-        profits_prev = [0] * prices_len
         profits = [0] * prices_len
 
         for i in range(k):
-            max_profits_prev = -float('inf')
-            profits = [0] * prices_len
+            max_profits = -float('inf')
+            new_profits = [0] * prices_len
             for j in range(1, prices_len):
-                max_profits_prev = max(max_profits_prev, profits_prev[j-1]-prices[j-1])
-                profits[j] = max(profits[j-1], prices[j] + max_profits_prev)
-            profits_prev = list(profits)
+                max_profits = max(max_profits, profits[j - 1] - prices[j - 1])
+                new_profits[j] = max(new_profits[j - 1], prices[j] + max_profits)
+            profits = list(new_profits)
 
         return max(profits)
 
